c4ffe505a72c0c7cfa67c46a791ba6543aa28e78
Dmitry <usefullforit@gmail.com>
2025-02-03 14:49:08 +0300

    Add game brain-prime

---
+++ src/Games/brain-prime.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bin\BrainGcd;
+
+require_once __DIR__ . '/../Engine.php';
+
+function isPrime(int $number): bool
+{
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+	return true;
+}
+function gamePrime(): void
+{
+    $userName = '';
+    $scores = 0;
+    $isRightAnswer = true;
+    showGreeting($userName);
+    showRule("Answer \"yes\" if given number is prime. Otherwise answer \"no\".");
+    while (($scores !== 3) && $isRightAnswer) {
+        $randomNumber = rand(1, 10);
+        askQuestion("Question: $randomNumber");
+        $userAnswer = enterUserAnswer();
+        $answer = isPrime($randomNumber) ? "yes" : "no";
+        makeComparison($userAnswer, $answer, $userName, $scores, $isRightAnswer);
+    }
+}
+
+gamePrime();
