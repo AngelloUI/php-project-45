61d125a1baaeb8cd8737ebef2e767aa609c794bf
Dmitry <usefullforit@gmail.com>
2025-02-03 05:17:22 +0300

    Add game brain-calc

--- src/Engine.php
+++ src/Engine.php
@@ -11,6 +11,7 @@ function getGreeting($name): string
 {
     return "Hello, {$name}!";
 }
+
 function showGreeting(string &$name): void
 {
     line('Welcome to the Brain Games!');
@@ -34,14 +35,17 @@ function showRule(string $rule): void
 {
     line($rule);
 }
-function showWrongAnswer(string $userAnswer, string $answer, string $userName): void
+
+function showWrongAnswer(string|int $userAnswer, string|int $answer, string $userName): void
 {
     line("'$userAnswer' is wrong answer ;(. Correct answer was '$answer'.\nLet's try again, $userName!");
 }
+
 function showCorrectAnswer(): void
 {
     line("Correct!");
 }
+
 function showUserWin(string $userName): void
 {
     line("Correct!\nCongratulations, $userName!");
@@ -52,12 +56,12 @@ function askQuestion($question): void
     line($question);
 }
 
-function enterUserAnswer(): string
+function enterUserAnswer(): string|int
 {
     return prompt('Your answer');
 }
 
-function makeComparison(string $userAnswer, string $answer, string $userName, int &$scores, bool &$isRightAnswer): void
+function makeComparison(string|int $userAnswer, string|int $answer, string $userName, int &$scores, bool &$isRightAnswer): void
 {
     if ($userAnswer === $answer) {
         $scores++;
